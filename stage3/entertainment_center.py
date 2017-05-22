4ed9ee366d1e5128f51e6c63cbe93e37bf416511
aaron stutzer <astutzer@zug-l-astutzer.local>
2017-05-22 18:42:56 +0200

    added descriptions

--- stage3/entertainment_center.py
+++ stage3/entertainment_center.py
@@ -1,6 +1,8 @@
 import fresh_tomatoes
 import media
 
+#list of movie with information
+
 blow = media.Movie("Blow", " biographical crime film about the American cocaine smuggler George Jung","https://upload.wikimedia.org/wikipedia/en/b/bf/Blow_poster.jpg", "https://www.youtube.com/watch?v=scWkP1GdnuU")
 entourage = media.Movie("Entourage","about a movie star and his posse","https://upload.wikimedia.org/wikipedia/en/d/df/Entourage_film_2015_poster.jpg","https://www.youtube.com/watch?v=SGSE_XPF4_g")
 training_day = media.Movie("Training Day","1st day of an officer in south central LA","https://upload.wikimedia.org/wikipedia/en/b/b3/Training_Day_Poster.jpg","https://www.youtube.com/watch?v=DXPJqRtkDP0")
@@ -8,6 +10,7 @@ law_abiding_citizen = media.Movie("Law Abiding Citizen","A man seeks justice for
 any_given_sunday = media.Movie("Any Given Sunday","Life is a contact sport","https://upload.wikimedia.org/wikipedia/en/8/80/Any_Given_Sunday.jpg","https://www.youtube.com/watch?v=a3nGztzo6Dk")
 the_shwawshank_redemption = media.Movie("The Shawshank Redemption","fear can hold you prisoner. hope can set you free","https://upload.wikimedia.org/wikipedia/en/8/81/ShawshankRedemptionMoviePoster.jpg","https://www.youtube.com/watch?v=6hB3S9bIaco")
 
+#attributes for fresh_tomatoes function
 movies = [blow, entourage, training_day, law_abiding_citizen, any_given_sunday, the_shwawshank_redemption]
+
 fresh_tomatoes.open_movies_page(movies)
-#(media.Movie.VALID_RATINGS)
--- stage3/media.py
+++ stage3/media.py
@@ -1,13 +1,16 @@
 import webbrowser
 
+#create class Movie with objects title storyline image and trailer
 class Movie():
     VALID_RATINGS = ["G","PG","PG-13","R"]
 
+    #initalize objects
     def __init__(self, movie_title, movie_storyline, poster_image, trailer_youtube):
         self.title = movie_title
         self.storyline = movie_storyline
         self.poster_image_url = poster_image
         self.trailer_youtube_url = trailer_youtube
 
+    #function to show movie trailer
     def show_trailer(self):
         webbrowser.open(self.trailer_youtube_url)
