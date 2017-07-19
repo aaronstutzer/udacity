e35fb56ebe028402610fc9e146761d25c86d1232
aaronstutzer <aaron.stutzer@ricardo.ch>
2017-07-19 06:44:07 +0200

    frontend resume

---
+++ iOS/variables-practice.playground/Contents.swift
@@ -0,0 +1,18 @@
+//: Playground - noun: a place where people can play
+
+
+// single line comment
+
+/* 
+ 
+ multi
+ line
+ comment
+ 
+ */
+
+//: markup comment
+
+import UIKit
+
+var str = "Hello, playground"
